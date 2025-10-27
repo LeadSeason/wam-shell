582a9fc03283e761fb24c54419a2cbe7918e31f9
Montazar <github.v0its@slmail.me>
2025-10-27 03:25:11 +0100

    Handles activation and menu display on click

--- widget/Tray.tsx
+++ widget/Tray.tsx
@@ -1,5 +1,5 @@
 import { createState, For } from "ags"
-import { Gtk } from "ags/gtk4"
+import { Gdk, Gtk } from "ags/gtk4"
 import app from "ags/gtk4/app"
 import AstalTray from "gi://AstalTray"
 
@@ -30,10 +30,31 @@ export default function Tray() {
         <For each={trayItems}>
           {(item) => (
             <menubutton
-              $={(self) =>
+              $={(self) => {
                 self.insert_action_group("dbusmenu", item.get_action_group())
-              }
+                const gestureClick = new Gtk.GestureClick({
+                  button: 0, // Listen to all buttons.
+                })
+
+                gestureClick.connect("pressed", (event) => {
+                  // Prevent default behavior.
+                  event.set_state(Gtk.EventSequenceState.CLAIMED)
+
+                  switch (event.get_current_button()) {
+                    case Gdk.BUTTON_PRIMARY:
+                      item.activate(0, 0)
+                      break
+                    case Gdk.BUTTON_SECONDARY:
+                      self.get_popover()?.popup()
+                      break
+                    default:
+                  }
+                })
+
+                self.add_controller(gestureClick)
+              }}
               tooltipMarkup={item.tooltipMarkup}
+              direction={Gtk.ArrowType.DOWN}
             >
               <image gicon={item.get_gicon()} tooltip-text={item.get_title()} />
               {Gtk.PopoverMenu.new_from_model(item.get_menu_model())}
