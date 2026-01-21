64bf6359bafc38663811813573b1efcf33e04d95
LeadSeason <admin@leadseason.eu>
2026-01-21 22:11:51 +0200

    Small little badge overlayed

--- src/widgets/bar/barModules/swayNC.tsx
+++ src/widgets/bar/barModules/swayNC.tsx
@@ -1,33 +1,46 @@
+import { execAsync } from "ags/process";
 import Gtk from "gi://Gtk?version=4.0";
-import SwayNc from "../../../lib/swayNC"
-import { execAsync } from "ags/process"
-import { createBinding } from "ags";
+import SwayNc from "../../../lib/swayNC";
+import { createBinding } from "gnim";
+import Adw from "gi://Adw?version=1";
 
 export default function Notify() {
     const swayNc = SwayNc.get_default();
 
-    const handleClick = (e: Gtk.Button) => {
+    const handleClick = (e: Gtk.GestureClick) => {
         execAsync("swaync-client -t -sw")
     }
     const reveal = createBinding(swayNc, "count").as((v) => v > 0)
     const count = createBinding(swayNc, "count").as((v) => v.toString())
-    const dnd = createBinding(swayNc, "dnd").as((v) => v ? "" : "") // TODO, use a icon instead of nerdfonts.
+    const icon = createBinding(swayNc, "dnd").as(
+        (v) => v ? "notifications-disabled-symbolic" : "notifications-symbolic"
+    )
+    const badge = (<revealer
+            transitionDuration={250}
+            transitionType={Gtk.RevealerTransitionType.SWING_DOWN}
+            revealChild={reveal}
+            valign={Gtk.Align.START}
+            halign={Gtk.Align.END}
+            marginEnd={5}
+        >
+            <label
+                hexpand={false}
+                vexpand={false}
+                class="Badge"
+                label={count} />
+        </revealer>) as Gtk.Box
 
-    return <button cssClasses={["swayNC"]}
-        onClicked={handleClick}
-    >
-        <box spacing={4}>
-            <revealer
-                transitionDuration={250}
-                transitionType={Gtk.RevealerTransitionType.SLIDE_RIGHT}
-                revealChild={reveal}
-            >
-                <label
-                    class="Badge"
-                    label={count} />
-            </revealer>
-                    
-            <label label={dnd}/>
-        </box>
-    </button>
+    return <box cssClasses={["swayNC"]}>
+        <Gtk.GestureClick
+            button={1}
+            onPressed={handleClick}
+        />
+        <overlay 
+            $={(self) => {
+                self.add_overlay(badge)
+            }}
+        >
+            <image iconName={icon} />
+        </overlay>
+    </box>
 }
