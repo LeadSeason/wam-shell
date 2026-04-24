4e27dcd246ed56859bb47bdfae0e34077c054091
LeadSeason <admin@leadseason.eu>
2026-04-24 22:54:28 +0300

    Check trust before updateing

--- scripts/archlinux-update.sh
+++ scripts/archlinux-update.sh
@@ -11,9 +11,13 @@ log() {
     printf "\033[1;34m[update]\033[0m %s\n" "$1"
 }
 
+log "updating trust..."
+
+sudo pacman -Sy archlinux-keyring --needed --noconfirm
+
 log "Starting full system update..."
 
-sudo pacman -Syu --noconfirm
+sudo pacman -Su --noconfirm
 
 if command -v yay >/dev/null 2>&1; then
     log "Updating AUR packages..."
